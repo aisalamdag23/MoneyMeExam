453c8cdefcb7a9d9025e38bca57973f057bffdf0
Aisa Lamdag - PDAX <aisa.lamdag@pdax.ph>
2023-07-11 15:18:30 +0800

    ADDED go mod and sum

--- api/handlers/post.go
+++ api/handlers/post.go
@@ -1,7 +1,13 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/aisalamdag23/MoneyMeExam/api/structs"
+	"github.com/gin-gonic/gin"
+)
 
 func PostLoanRequest(ctx *gin.Context) {
+	var request structs.PostLoanRequest
+	if err := ctx.Bind(&request); err != nil {
 
+	}
 }
